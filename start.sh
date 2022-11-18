9e97fb1bbed1658108852abf87396bf83571d857
lalyos <lalyos@yahoo.com>
2022-11-18 11:41:58 +0100

    use _FILE aware config (from postgres)

--- start.sh
+++ start.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+file_env TITLE 'Hello-file-aware'
+
 cat > /var/www/html/index.html <<EOF
 <html>
   <body bgcolor="${COLOR:-gray}">
-    <h1>${TITLE:-Hello}</h1>
+    <h1>${TITLE}</h1>
     ${BODY:-Please use TITLE/COLOR/BODY env vars}
   </body>
+  <hr>
+  Hostname: ${HOSTNAME}
 <html>
 EOF
 
